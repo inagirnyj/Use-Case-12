12f7b526f21caebf6ab75b1d380e6649d80ec720
Ihor Nahirnyi <133789953+in2488@users.noreply.github.com>
2023-09-11 19:54:57 +0300

    UC#12 Create documentation

--- CommentEndpoint.java
+++ CommentEndpoint.java
@@ -9,21 +9,47 @@ import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.comment.CommentDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
 
+/**
+ * This class represents the endpoint for managing comment-related operations.
+ * It provides methods for creating, updating, retrieving, and listing comments.
+ * @since 11Sep2023
+ */
 public class CommentEndpoint extends AbstractWebEndpoint {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String COMMENTS_END = "/comments";
     private static final String COMMENTS_RESOURCE_END = "/comments/{commentID}";
 
+    /**
+     * Constructs a new CommentEndpoint instance with the given specification.
+     *
+     * @param specification The RequestSpecification used for making HTTP requests
+     * @author Ihor Nahirnyi
+     */
     public CommentEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+     * Creates a new comment with the provided CommentDto.
+     *
+     * @param commentDto The CommentDto representing the comment to create.
+     * @return The created CommentDto
+     * @author Ihor Nahirnyi
+     */
     public CommentDto create(CommentDto commentDto) {
         return create(commentDto, HttpStatus.CREATED)
             .extract().as(CommentDto.class);
     }
 
+    /**
+     * Creates a new comment with the provided CommentDto and validates the HTTP status code.
+     *
+     * @param commentDto The CommentDto representing the comment to create.
+     * @param status     The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse create(CommentDto commentDto, HttpStatus status) {
         LOGGER.info("Create new Comment");
         return post(
@@ -33,11 +59,28 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Updates an existing comment with the provided CommentDto.
+     *
+     * @param id         The ID of the comment to update.
+     * @param commentDto The CommentDto representing the updated comment data.
+     * @return The updated CommentDto
+     * @author Ihor Nahirnyi
+     */
     public CommentDto update(int id, CommentDto commentDto) {
         return update(commentDto, id, HttpStatus.OK)
             .extract().as(CommentDto.class);
     }
 
+    /**
+     * Updates an existing comment with the provided CommentDto and validates the HTTP status code.
+     *
+     * @param commentDto The CommentDto representing the updated comment data.
+     * @param id         The ID of the comment to update.
+     * @param status     The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse update(CommentDto commentDto, int id, HttpStatus status) {
         LOGGER.info("Update Comment by id [{}]", id);
         return put(
@@ -48,11 +91,26 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves a comment by its ID.
+     *
+     * @param id The ID of the comment to retrieve.
+     * @return The CommentDto representing the retrieved comment
+     * @author Ihor Nahirnyi
+     */
     public CommentDto getById(int id) {
         return getById(id, HttpStatus.OK)
             .extract().as(CommentDto.class);
     }
 
+    /**
+     * Retrieves a comment by its ID and validates the HTTP status code.
+     *
+     * @param id     The ID of the comment to retrieve.
+     * @param status The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse getById(int id, HttpStatus status) {
         LOGGER.info("Get Comment by id [{}]", id);
         return get(
@@ -62,15 +120,27 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves a list of all comments.
+     *
+     * @return A list of CommentDto objects representing all comments
+     * @author Ihor Nahirnyi
+     */
     public List<CommentDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(CommentDto[].class));
     }
 
+    /**
+     * Retrieves a list of all comments and validates the HTTP status code.
+     *
+     * @param status The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Comments");
         ValidatableResponse response = get(this.specification, COMMENTS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
 }
--- UserEndpoint.java
+++ UserEndpoint.java
@@ -9,21 +9,47 @@ import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.user.UserDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
 
+/**
+ * This class represents the endpoint for managing user-related operations.
+ * It provides methods for creating, updating, retrieving, and listing users.
+ * @since 11Sep2023
+ */
 public class UserEndpoint extends AbstractWebEndpoint {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String USERS_END = "/users";
     private static final String USERS_RESOURCE_END = "/users/{userID}";
 
+    /**
+     * Constructs a new UserEndpoint instance with the given specification.
+     *
+     * @param specification The RequestSpecification used for making HTTP requests.
+     * @author Ihor Nahirnyi
+     */
     public UserEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+     * Creates a new user with the provided UserDto.
+     *
+     * @param userDto The UserDto representing the user to create.
+     * @return The created UserDto.
+     * @author Ihor Nahirnyi
+     */
     public UserDto create(UserDto userDto) {
         return create(userDto, HttpStatus.CREATED)
             .extract().as(UserDto.class);
     }
 
+    /**
+     * Creates a new user with the provided UserDto and validates the HTTP status code.
+     *
+     * @param userDto The UserDto representing the user to create.
+     * @param status  The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation.
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse create(UserDto userDto, HttpStatus status) {
         LOGGER.info("Create new User");
         return post(
@@ -33,11 +59,28 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Updates an existing user with the provided UserDto.
+     *
+     * @param id      The ID of the user to update.
+     * @param userDto The UserDto representing the updated user data.
+     * @return The updated UserDto.
+     * @author Ihor Nahirnyi
+     */
     public UserDto update(int id, UserDto userDto) {
         return update(userDto, id, HttpStatus.OK)
             .extract().as(UserDto.class);
     }
 
+    /**
+     * Updates an existing user with the provided UserDto and validates the HTTP status code.
+     *
+     * @param userDto The UserDto representing the updated user data.
+     * @param id      The ID of the user to update.
+     * @param status  The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation.
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse update(UserDto userDto, int id, HttpStatus status) {
         LOGGER.info("Update User by id [{}]", id);
         return put(
@@ -48,11 +91,26 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves a user by ID.
+     *
+     * @param id The ID of the user to retrieve.
+     * @return The UserDto representing the retrieved user.
+     * @author Ihor Nahirnyi
+     */
     public UserDto getById(String id) {
         return getById(id, HttpStatus.OK)
             .extract().as(UserDto.class);
     }
 
+    /**
+     * Retrieves a user by ID and validates the HTTP status code.
+     *
+     * @param id     The ID of the user to retrieve.
+     * @param status The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation.
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse getById(String id, HttpStatus status) {
         LOGGER.info("Get User by id [{}]", id);
         return get(
@@ -62,15 +120,27 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return A list of UserDto objects representing all users.
+     * @author Ihor Nahirnyi
+     */
     public List<UserDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(UserDto[].class));
     }
 
+    /**
+     * Retrieves a list of all users and validates the HTTP status code.
+     *
+     * @param status The expected HTTP status code.
+     * @return A ValidatableResponse containing the HTTP response for validation.
+     * @author Ihor Nahirnyi
+     */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Users");
         ValidatableResponse response = get(this.specification, USERS_END);
         response.statusCode(status.getCode());
         return response;
     }
-
 }
